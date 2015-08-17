95806f19478c3181c484842561005766f4911461
tarky <taisuke.araki@gmail.com>
2015-08-17 10:53:50 +0900

    Change file path to "/etc/profile.d".
    Change file path to "/etc/profile.d" in order to use in cron.

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,7 +8,7 @@
 #
 
 node.deploy.each_pair do |app_name,vals|
-  file File.join(vals[:home], "shellinit-#{app_name}.sh") do
+  file File.join("/etc/profile.d/", "shellinit-#{app_name}.sh") do
     envs = []
     vals.environment_variables.each_pair {|key, val| envs << "export #{key}=\"#{val}\""} if vals.environment_variables
     owner vals[:user]
